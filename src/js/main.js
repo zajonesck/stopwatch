7674064d1c89a3d1bdb9f098c0f11623e5eabe71
Zack Jones <zajonesck83@gmail.com>
2022-12-15 22:55:41 -0600

    more js logic still need to fix the second counter and add minutes

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,48 @@ window.onload = function () {
   var minutes = 00;
   var seconds = 00;
   var tenths = 00;
+  var appendTenths = document.getElementById("tenths");
+  var appendSeconds = document.getElementById("seconds");
+  var appendMinutes = document.getElementById("minutes");
   var buttonStart = document.getElementById("button-start");
   var buttonStop = document.getElementById("button-stop");
   var buttonReset = document.getElementById("button-reset");
+  var Interval;
+
+  buttonStart.onclick = function () {
+    clearInterval(Interval);
+    Interval = setInterval(startTimer, 10);
+  };
+  buttonStop.onclick = function () {
+    clearInterval(Interval);
+  };
+  buttonReset.onclick = function () {
+    clearInterval(Interval);
+    tenths = "00";
+    seconds = "00";
+    minutes = "00";
+    appendTenths.innerHTML = tenths;
+    appendSeconds.innerHTML = seconds;
+    appendMinutes.innerHTML = minutes;
+  };
+  function startTimer() {
+    tenths++;
+
+    if (tenths <= 9) {
+      appendTenths.innerHTML = "0" + tenths;
+    }
+    if (tenths > 9) {
+      appendTenths.innerHTML = tenths;
+    }
+    if (tenths > 99) {
+      console.log("seconds");
+      seconds++;
+      appendSeconds.innerHTML = "0" + seconds;
+      tenths = 0;
+      appendTenths.innerHTML = "0" + 0;
+    }
+    if (seconds > 9) {
+      appendSeconds.innerHtml = seconds;
+    }
+  }
 };
