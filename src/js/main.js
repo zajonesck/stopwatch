a79265278963a0935bfda0d33d30a6f95d9ea3d1
Zack Jones <zajonesck83@gmail.com>
2022-12-15 21:42:19 -0600

    some JS logic

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,8 @@
-alert("Stopwatch!!");
+window.onload = function () {
+  var minutes = 00;
+  var seconds = 00;
+  var tenths = 00;
+  var buttonStart = document.getElementById("button-start");
+  var buttonStop = document.getElementById("button-stop");
+  var buttonReset = document.getElementById("button-reset");
+};
